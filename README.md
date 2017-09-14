806c8652b069cdb6e69fe28bdc39fca03a15ba2b
Chinegua <ai.chinegua@gmail.com>
2017-09-14 14:14:42 +0200

    Creando clase y cambiando POM

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ Este proyecto pretende ser una plantilla maven para crear un proyecto
 1.- Editar el pom.xml y cambiarle el nombre del proyecto
 1.- Desde Eclipse, importar el proyecto como un proyecto de maven existente
 
+# Punto 2
+Los niveles de logs son configurables desde el fichero POM.xml en el apartado de los perfiles.
+
---
+++ src/main/java/aitor/bernal/falcon/SVC/miw/upm/es/log/LoggerDemo.java
@@ -0,0 +1,19 @@
+package aitor.bernal.falcon.SVC.miw.upm.es.log;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class LoggerDemo {
+    public void logs() {
+        Logger logger = LogManager.getLogger(this.getClass().getName());
+        logger.debug("Log de debug");
+        logger.info("Log de info");
+        logger.error("Log de error");
+    }
+
+    public static void main(String[] args) {
+        new LoggerDemo().logs();
+    }
+
+}
