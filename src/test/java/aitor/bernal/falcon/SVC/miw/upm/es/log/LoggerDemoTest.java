7472b23a7c2d869de0ff3398629106652e88a339
Chinegua <ai.chinegua@gmail.com>
2017-09-15 17:08:42 +0200

    Update travis.yml

---
+++ src/test/java/aitor/bernal/falcon/SVC/miw/upm/es/log/LoggerDemoTest.java
@@ -0,0 +1,13 @@
+package aitor.bernal.falcon.SVC.miw.upm.es.log;
+
+import org.junit.Test;
+
+public class LoggerDemoTest {
+    
+    @Test
+    public void testLogs() {
+        new LoggerDemo().logs();
+    }
+
+
+}
